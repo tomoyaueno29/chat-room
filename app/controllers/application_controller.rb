4786db4652d022db39f57d333af8b863f579d999
tomoyaueno29 <u.t01280729@gmail.com>
2020-04-22 19:50:52 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    if resource_class == User
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    elsif resource_class == Shop
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    else
+      super
+    end
+  end
 end
