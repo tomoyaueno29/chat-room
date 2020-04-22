caf557edce806f6f70015232f39ee5305a16dc0a
tomoyaueno29 <u.t01280729@gmail.com>
2020-04-22 21:45:31 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'rooms/show'
-  get 'tops/index'
+  root 'tops#index'
   devise_for :shops
   devise_for :users
+  resources :rooms, :only => [:show, :create] do
+    resources :messages, :only => [:create]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
